7eac8a314c6006e8e2e7f3bd537f04385b2db553
Yoimor <94640525+Yoimor@users.noreply.github.com>
2020-09-27 16:04:04 +0000

    Updated the xlwings feedstock.

--- stockapi.h
+++ stockapi.h
@@ -7,4 +7,4 @@ static size_t WriteMemoryCallback
 
 	BufferStruct * mem = (BufferStruct *) data;
 
-	mem->buffer = (char*) realloc(mem->buffer, mem->size + reals
+	mem->buffer = (char*) realloc(mem->buffer, mem->size + realsize + 1
