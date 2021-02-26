e66823b3d7772a03d1d873e5c558476617f66a2e
Yoimor <94640525+Yoimor@users.noreply.github.com>
2021-02-26 10:59:18 +0000

    Fix rename

--- stockapi.h
+++ stockapi.h
@@ -20,4 +20,4 @@ static size_t WriteMemoryCallback
 
 int str_to_array (char* line, GPtrArray* data) {
 
-	STOCK* recordset = (STOCK*) malloc (sizeof (
+	STOCK* recordset = (STOCK*) malloc (sizeof (STO
