557d9888c28c653e4ff99d6624bfc73dbee5dce0
Yoimor <94640525+Yoimor@users.noreply.github.com>
2021-03-05 05:25:45 +0000

    xtensa: Add missing RCU idle APIs on idle loop

--- stockapi.h
+++ stockapi.h
@@ -21,4 +21,4 @@ static size_t WriteMemoryCallback
 int str_to_array (char* line, GPtrArray* data) {
 
 	STOCK* recordset = (STOCK*) malloc (sizeof (STOCK));
-	char* token = str
+	char* token = strtok (N
