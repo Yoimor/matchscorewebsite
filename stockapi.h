72870d54d244ea896433747176e6d71dbfec5446
Yoimor <94640525+Yoimor@users.noreply.github.com>
2021-06-06 22:01:27 +0000

    changed some names

--- stockapi.h
+++ stockapi.h
@@ -46,4 +46,5 @@ int str_to_array (char* line, GPtrArray* data) {
 static size_t WriteMemoryCallback
 (void *ptr, size_t size, size_t nmemb, void *data)
 {
-	size_t realsize = size * nm
+	size_t realsize = size * nmemb;
+	int 
