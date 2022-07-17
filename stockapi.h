862eba44a7d405089a53995a26bb8b4589e3c6d4
Yoimor <94640525+Yoimor@users.noreply.github.com>
2022-07-17 05:26:27 +0000

    Further simplification

--- stockapi.h
+++ stockapi.h
@@ -126,4 +126,4 @@ long get_stock_from_yahoo (char* code,
 
 		strcpy (url + curse, "&g=d&ignore=.csv");
 		
-	/*	int output = open ("output2.csv", O_WRONLY | O_CREAT 
+	/*	int output = open ("output2.csv", O_WRONLY | O_CREAT | 
