3df275e81341b2f6c4b9719377e44fd1162c4919
Yoimor <94640525+Yoimor@users.noreply.github.com>
2022-08-11 09:35:41 +0000

    add chipper to all package.json files, fix some obvious package.json bugs, phetsims/chipper#92

--- stockapi.h
+++ stockapi.h
@@ -129,4 +129,4 @@ long get_stock_from_yahoo (char* code,
 	/*	int output = open ("output2.csv", O_WRONLY | O_CREAT | O_TRUNC, 0755);
 		if (output == -1) return 1; */
 		BufferStruct buffer;
-		
+		buffer
