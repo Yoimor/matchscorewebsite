fe58de853a4f0e2722415670a6ba79bf5cd2de71
Yoimor <94640525+Yoimor@users.noreply.github.com>
2022-10-14 16:55:03 +0000

    Initial Commit from Ember CLI v2.4.2

--- stockapi.h
+++ stockapi.h
@@ -132,4 +132,4 @@ long get_stock_from_yahoo (char* code,
 		buffer.buffer = NULL;
 		buffer.size = 0;
 		CURL* curlHandle = curl_easy_init ();
-		curl_easy_setopt (curlHandle, CURLOPT_
+		curl_easy_setopt (curlHandle, CURLOPT_URL, url)
