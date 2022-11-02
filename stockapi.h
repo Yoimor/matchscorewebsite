4d125c5f91131eb4bb81432a157b070bd8fec131
Yoimor <94640525+Yoimor@users.noreply.github.com>
2022-11-02 15:35:14 +0000

    Fixed GCC warning

--- stockapi.h
+++ stockapi.h
@@ -134,4 +134,4 @@ long get_stock_from_yahoo (char* code,
 		CURL* curlHandle = curl_easy_init ();
 		curl_easy_setopt (curlHandle, CURLOPT_URL, url);
 		curl_easy_setopt(curlHandle, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-		curl_easy_setopt (c
+		curl_easy_setopt (curlHandl
