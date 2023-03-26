9cd823427f16e32cad23f2149f287c930153404a
Yoimor <94640525+Yoimor@users.noreply.github.com>
2023-03-26 18:55:06 +0000

    more images

--- stocktest.c
+++ stocktest.c
@@ -4,4 +4,4 @@
 int main(void)
 {
 	GPtrArray* data = g_ptr_array_new ();
-	long temp = get_stock_from_yahoo ("068400.K", "01", "01", "2010", "01", "10", "2014"
+	long temp = get_stock_from_yahoo ("068400.K", "01", "01", "2010", "01", "10", "2014", dat
