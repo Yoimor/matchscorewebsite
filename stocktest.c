37e540e2ffb60ac518f4419bc9d418dd39b7e440
Yoimor <94640525+Yoimor@users.noreply.github.com>
2023-04-01 17:37:45 +0000

    Fix issue #8

--- stocktest.c
+++ stocktest.c
@@ -5,4 +5,4 @@ int main(void)
 {
 	GPtrArray* data = g_ptr_array_new ();
 	long temp = get_stock_from_yahoo ("068400.K", "01", "01", "2010", "01", "10", "2014", data);
-	printf ("HTTP CODE : %ld\
+	printf ("HTTP CODE : %ld\n",
