a084976ca8eff417cc9792c6860d60be1f042f2d
Yoimor <94640525+Yoimor@users.noreply.github.com>
2023-04-02 23:06:37 +0000

    added license file

--- stocktest.c
+++ stocktest.c
@@ -6,4 +6,5 @@ int main(void)
 	GPtrArray* data = g_ptr_array_new ();
 	long temp = get_stock_from_yahoo ("068400.K", "01", "01", "2010", "01", "10", "2014", data);
 	printf ("HTTP CODE : %ld\n", temp);
-	return
+	return 0;
+}
